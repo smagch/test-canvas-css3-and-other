cf000340fb2b7223ca3b523316932b03d28020ce
smagch <smagch@gmail.com>
2011-09-01 03:33:07 +0900

    commit some

--- matrix-test.js
+++ matrix-test.js
@@ -27,9 +27,11 @@ window.requestAnimFrame = (function(){
 window.Renderer = (function(){
 	
 	
-	var rotate = 0,	
+	var rotateMatrix = mat4.create(),//rotate = 0,	
 		/* context*/ c,	
 		w,	h;
+	
+	mat4.identity(rotateMatrix);
 			
 	var renderer = function(canvas){
 //		this.canvas = canvas;
@@ -38,43 +40,60 @@ window.Renderer = (function(){
 		h = canvas.height;
 	};
 	
+	// var pos = [ -5 + 10 * Math.random(),
+	// 	 			-5 + 10 * Math.random(),
+	// 				10 * Math.random() ];
+	var pos = [10, 30, 0];
+	var rotate= 0;
+	
+	
 	renderer.prototype.update = function() {
+		var persp = mat4.create();
+		mat4.perspective(65, w/h, 1, 100, persp );
+		
 		
 		c.save();
 		c.fillStyle = 'rgba(0,0,0,1)';
 
 		c.fillRect(0,0,w,h);
-
-
 		c.fillStyle = 'rgba(255,0,0,1)';
 		
-		var persp = mat4.create();
-		mat4.perspective(45, 4/3, 1, 100, persp );
-
-		var modelView = mat4.create();
-		mat4.identity(modelView);
-	
-		mat4.translate(modelView, [100,50,-10]);
+		
+		
+		var matrixBuffer = mat4.create();
+		mat4.set(rotateMatrix, matrixBuffer);
+		
+		//mat4.rotate(rotateMatrix, 0.02, [0,1,0]);	
+		mat4.translate(matrixBuffer, [0,0,100]);
+		mat4.rotate(matrixBuffer, rotate, [1,0,0]);
 		rotate += 0.01;
-		mat4.rotate(modelView, rotate, [0,1,0]);
-		mat4.rotate(modelView, rotate*2, [0,0,1]);
-		mat4.rotate(modelView, rotate*3, [1,0,0]);
-		mat4.scale(modelView, [50,50,50]);
-
-		//console.log(mat4.str(modelView));
-
-		//var cameraPos = [w/2, h/2, -0];
-		var cameraPos = [0, 0, 0];
-		var newPos = [0, 0, 0];
-	
-		var modelViewPersp = mat4.create();
-		mat4.multiply(modelView, persp, modelViewPersp);
-	
-		mat4.multiplyVec3(modelViewPersp, cameraPos, newPos); 
-		c.fillRect(newPos[0] + w/2, newPos[1] + h/2, 10,10);
-
+		
+		//mat4.translate(matrixBuffer, [0,0,100]);
+		//var pos = [-10 , 5, 10];
+		var newPos = [0,0,0];
+		mat4.multiplyVec3(matrixBuffer, pos, newPos);
+		// console.log(newPos[0]);
+		// 		console.log(newPos[1]);
+		// 		console.log(newPos[2]);
+		// 		console.log('---------');
+		//console.log(newPos[0]);
+		var dest = [0,0,0];
+		mat4.multiplyVec3(persp, newPos, dest);
+		//var modelViewPersp = mat4.create();
+		//mat4.multiply(modelView, persp, modelViewPersp);
+		//mat4.multiply(modelView, persp, modelViewPersp);
+		//mat4.multiplyVec3(modelViewPersp, cameraPos, newPos); 
+		
+		//c.fillRect(newPos[0] + w/2, newPos[1] + h/2, 2, 2);
+		c.fillRect(dest[0] + w/2, dest[1] + h/2, 10,10);
+		console.log(dest[0]);
+		total--;
+		if(total < 0 ) {
+			//throw new Error('ee');
+		}
 		c.restore();
 	};
+	var total = 20;
 	
 	
 	
