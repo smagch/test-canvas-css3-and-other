dc1979d97b402ffc90f9c1b8914b175f20e04590
smagch <smagch@gmail.com>
2011-09-04 13:05:03 +0900

    add 3 and 4

--- matrix-test3.js
+++ matrix-test3.js
@@ -2,7 +2,7 @@
 function initCanvas(canvas) {
 	
 	var render = new Renderer(canvas);
-	
+
 	(function animloop(){
 	      render.update();
 	      requestAnimFrame(animloop);
@@ -29,9 +29,11 @@ window.Renderer = (function(){
 	
 	var NUM_ELEMENTS = 100,
 		rotate = 0,
+		rotateX = 0,
+		rotateY = 0,
 		persp = mat4.create(),
 		/* context*/ c,	
-		w,	h,
+		w, h, x, y,
 		pos = new glMatrixArrayType(),
 		newPos = new glMatrixArrayType(),
 		dest = new glMatrixArrayType();
@@ -52,23 +54,59 @@ window.Renderer = (function(){
 		w = canvas.width;
 		h = canvas.height;
 		mat4.perspective(65, w/h, 100, 200, persp );
+		
+		x = canvas.offsetLeft;
+		y = canvas.offsetTop;
+
+		canvas.onmousemove = function(event) {
+			//console.log( ' x : ' + (event.screenX - canvas.offsetLeft));
+			rotateX = Math.PI * (event.screenX - canvas.offsetLeft - w ) / w;
+			rotateY = Math.PI * (event.screenY - canvas.offsetTop - h ) / h;
+			//eventTest++;
+		};
+		if(!window) {
+			return new Error('no window');
+		}
+		window.onmousedown = function(evnet) {
+			isMouseDown = true;
+		};
+		window.onmouseup = function(event) {
+			isMouseDown = false;
+		}
+		
 	};
-	
+	var isMouseDown = false;
+	//var eventTest = 0;
+
 	renderer.prototype.update = function() {
 		
+		// if(eventTest >= 1) {
+		// 			console.log('test : ' + eventTest);
+		// 			eventTest = 0;
+		// 		}
+		if(isMouseDown) {
+			console.log('mouseDown');
+		}
 		c.save();
 		c.fillStyle = 'rgba(0,0,0,1)';
-
 		c.fillRect(0,0,w,h);
+		
 		c.fillStyle = 'rgba(255,0,0,1)';
+		
+		
 		
 		var matrixBuffer = mat4.create();
 		mat4.identity(matrixBuffer);
-		
+	
 		mat4.translate(matrixBuffer, [0,0,100]);
+		
+		rotate++;
 		mat4.rotate(matrixBuffer, rotate * 0.02, [0,1,0]);
 		mat4.rotate(matrixBuffer, rotate * 0.01, [1,0,0]);
-		rotate++;
+
+		mat4.rotate(matrixBuffer, rotateX, [0,1,0]);
+		mat4.rotate(matrixBuffer, rotateY, [1,0,0]);
+				
 		
 		for( var i = 0; i < NUM_ELEMENTS ; i++) {
 			mat4.multiplyVec3(matrixBuffer, pos[i], newPos[i]);
